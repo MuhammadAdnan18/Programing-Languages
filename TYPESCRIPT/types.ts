3fc4353c50e411a0601addb9889213cdb2ef63e3
Adnan <muhammadadnanvia@gmail.com>
2022-11-05 11:28:30 +0500

    types

---
+++ TYPESCRIPT/types.ts
@@ -0,0 +1,9 @@
+/*two types of assignment in TS*/
+
+//Explicit type: in which u define the data type//
+
+let Name: String = "Ahmad";
+
+//emplicit type : no need to define data type //
+ let fname = 'hamza';
+ let k = 4 ;
