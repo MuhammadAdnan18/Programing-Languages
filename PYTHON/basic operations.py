172cdf3a013d5032b63d25970c6260fbe7c5eaeb
Adnan <muhammadadnanvia@gmail.com>
2022-11-08 18:23:34 +0500

    python1

---
+++ PYTHON/basic operations.py
@@ -0,0 +1,9 @@
+number1 = 64
+number2 = 8
+result = number1/number2
+print(result)
+
+
+x=float(input("Enter weight in kg "))
+w_p=x/2.2
+print( w_p)
---
+++ PYTHON/if-else.py
@@ -0,0 +1,12 @@
+
+bal=int(input("Enter Balance " ))
+if bal > 10:
+    print("your acccount is verified")
+
+elif bal == 1:
+    
+    print("Add more balance")
+
+else:
+    
+    print("your account was deleted because of  inactivity")
