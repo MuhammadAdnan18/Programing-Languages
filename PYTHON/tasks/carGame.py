bb4d82dd13bbe6dcaaeba12f9db2991971fb1030
Adnan <muhammadadnanvia@gmail.com>
2022-11-14 20:55:42 +0500

    car game & guess number

---
+++ PYTHON/tasks/carGame.py
@@ -0,0 +1,16 @@
+       #car engine game without GUI
+
+command = input("Give command ")
+
+if command=="start" :
+   print("Car has been started🚗" )
+
+elif command=="stop" :
+   print("Car has stopped 🚗")
+   
+elif command=="help" :
+   print("type start to start the car \n type stop to stop the car \n press X to exit the game")
+   
+elif command =="X" :
+   print("The game is finished")     
+  
