42eb9cf30e12344e1a8a1e9e81eeb4b7859e96b7
MuhammadAdnan18 <97541713+MuhammadAdnan18@users.noreply.github.com>
2023-01-16 19:54:49 +0500

    jaavscript

---
+++ JAVASCRIPT/Class.js
@@ -0,0 +1,14 @@
+//CLASSES
+
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+  }
+
+  let car1 = new Car("Honda",1992);
+  console.log(car1);
+
+  // program to Replicate the working of an ATM machine in c++?
+
