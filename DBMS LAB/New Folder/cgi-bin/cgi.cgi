848e070e6d9df47c8a3bba81744528024f96e2d6
MuhammadAdnan18 <97541713+MuhammadAdnan18@users.noreply.github.com>
2023-05-12 10:27:32 +0500

    extra files removed from dbms lab

--- DBMS LAB/New Folder/cgi-bin/cgi.cgi
+++
@@ -1,5 +0,0 @@
-#!"F:\uni\IQRA\courses\DBMS LAB\New folder\perl\bin\perl.exe"
-
-print "Content-Type: text/html\n\n";
-
-print "OK";
--- DBMS LAB/classes/CLASS4/testuser.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM employee
-
-INSERT INTO employee(e_name,salary,designation)
-VALUES ('kashif',3233,'faculty')
-
-UPDATE employee  
-SET designation ='sta'
-WHERE e_id=6
