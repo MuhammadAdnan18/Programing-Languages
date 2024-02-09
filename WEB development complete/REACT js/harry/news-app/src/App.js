a64190dfcadaacf184b14c5e1480d1608ec52021
Muhammad Adnan <muhammadadnanvia@gmail.com>
2024-02-09 16:46:22 +0500

    NewsBee completed

--- WEB development complete/REACT js/harry/news-app/src/App.js
+++ WEB development complete/REACT js/harry/news-app/src/App.js
@@ -2,15 +2,138 @@ import Navbar from "./components/Navbar";
 import "./App.css";
 import React, { Component } from "react";
 import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 //instead of built in function based component we create a classs based component
 //in class we can create multiple functions and use all features of oop and classes like constructor(this.name), inheritence and call functions
 export default class App extends Component {
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={6} />
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color="#A7BDC8"
+            progress={this.state.progress}
+            height={3}
+          />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="general"
+                  pageSize={6}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="business"
+                  pageSize={6}
+                  country="us"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="entertainment"
+                  pageSize={6}
+                  country="us"
+                  category="entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/general"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="general"
+                  pageSize={6}
+                  country="us"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="health"
+                  pageSize={6}
+                  country="us"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="science"
+                  pageSize={6}
+                  country="us"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="sports"
+                  pageSize={6}
+                  country="us"
+                  category="sports"
+                />
+              }
+            />
+
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="technology"
+                  pageSize={6}
+                  country="us"
+                  category="technology"
+                />
+              }
+            />
+          </Routes>
+        </Router>
       </div>
     );
   }
--- WEB development complete/REACT js/harry/news-app/src/components/NewsItem.js
+++ WEB development complete/REACT js/harry/news-app/src/components/NewsItem.js
@@ -1,32 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, src } = props;
 
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={
-              !imageUrl
-                ? "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i7aCeFTmU0Mg/v0/1200x799.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
-              Read More...
-            </a>
-          </div>
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !imageUrl
+              ? "https://as1.ftcdn.net/v2/jpg/04/54/38/16/1000_F_454381691_37sakwmAos8PKW2GcmplzsE6Q2oz5RIZ.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              by {author} |On {new Date(date).toGMTString()} |
+              <span className="badge bg-secondary">{src}</span>
+            </small>
+          </p>
+
+          <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+            Read More...
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
